b8e3890a66f2ca26a280a910d2c2c4b88c36a64f
userimack <mahendra.k12@gmail.com>
2016-03-06 16:00:05 +0530

    added modified emal code

--- email_send/email_send.py
+++ email_send/email_send.py
@@ -1,11 +1,28 @@
-# Send mail using python
+# Send mail using python with subject
 
-import smtplib 
+import smtplib
+import os
 
-content = "example email stuff here " 
-mail = smtplib.SMTP ("smtp.gmail.com",587)
-mail.ehlo()
-mail.starttls()
-mail.login('email' , "password" )
-mail.sendmail ("fromemail" , "receviver" , content)
-mail.close()﻿
+fromaddr = os.environ['email_id']
+toaddrs = ['mahendra.k12@gmail.com','mahendrarao741@gmail.com']  #to send a mail to  multiple user
+#toaddrs = 'mahendrarao741@gmail.com' #to send to a single user
+
+subject = 'Mail from Terminal'
+body = 'Hello From Terminal'
+
+msg = 'Subject: %s\n\n%s' % (subject, body)
+
+
+#Credentials
+username = os.environ['email_id']
+password = os.environ['email_passwd']
+
+# The actual mail send 
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
+
+print("Success")
+
