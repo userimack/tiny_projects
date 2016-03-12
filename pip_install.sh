eae9d7ac8b4b2b2df890585a01bc94799fd880e8
userimack <mahendra.k12@gmail.com>
2016-03-12 23:05:34 +0530

    added shell script

---
+++ pip_install.sh
@@ -0,0 +1,6 @@
+# Script to successfully install packages in a requirements.txt file . When all packages are installed then simply close the scipt by pressing ctrl + z 
+
+while (( 1 ))
+do
+pip install -r requirements.txt
+done
